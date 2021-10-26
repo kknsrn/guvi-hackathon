824aaab77356bc0e3ca4ac80c9d1bbb2d314624b
Saran <89548387+kknsrn@users.noreply.github.com>
2021-10-26 11:10:07 +0530

    makeupapi

--- script.js
+++ script.js
@@ -1,18 +1,17 @@
 const API_URL= "https://makeup-api.herokuapp.com/api/v1/products.json";
 const searchBtn = document.getElementById('search-btn');
 const makeUpList = document.getElementById('makeup');
-const makeUpDetailsContent = document.querySelector('.makeup-details-content');
+const makeUpDetailsContent = document.querySelector('.makeUp-details-content');
 const makeUpCloseBtn = document.getElementById('makeup-close-btn');
 
 // event listeners
 searchBtn.addEventListener('click', getMakeUpList);
-newFunction();
-function newFunction() {
-    makeUpList.addEventListener('click', getMakeUpDetails);
-    makeUpCloseBtn.addEventListener('click', () => {
-        makeUpDetailsContent.parentElement.classList.remove('showMakeUp');
-    });
-}
+makeUpList.addEventListener('click', getMakeUpDetails);
+makeUpCloseBtn.addEventListener('click', () => {
+makeUpDetailsContent.parentElement.classList.remove('showMakeUp');
+   
+});
+
 
 function getMakeUpList(){
     let searchInputTxt = document.getElementById(`search-input`).value.trim();
@@ -20,21 +19,22 @@ function getMakeUpList(){
     .then(response => response.json())
     .then(data => {
       let html = "";
-              if(data){
+        if(data){
              
-                 data.forEach( post =>  {
-                    html += `
+             data.forEach( makeUp =>  {
+                 html += `
                     
-                     <div class = "makeup-item" data-id = "${post.id}">
+                    <div class = "makeup-item" data-id = "${makeUp.id}">
                      
-                          <div class = "makeup-img">
-                             <img src = "${post.image_link}"> >
-                    </div>
+                            <div class = "makeup-img">
+                             <img src = "${makeUp.image_link}" alt src="img.jfif"> 
+                            </div>
                          
-                         <div class = "makeup-name">
-                             <h3>${post.name}</h3>
+                          <div class = "makeup-name">
+                             <h3>${makeUp.name}</h3>
+                             <h4 class="price">${makeUp.price_sign}${makeUp.price}</h4>
                              <a href = "#" class = "kit-btn">product details</a>
-                         </div>
+                             </div>
                           
                     </div>
                 `;
@@ -42,7 +42,7 @@ function getMakeUpList(){
         
             makeUpList.classList.remove('notFound');
         } else{
-            html = "Sorry, we didn't find any produts!",
+            html = "Sorry, we didn't find any produts!";
             makeUpList.classList.add('notFound');
         }
 
@@ -57,32 +57,35 @@ function getMakeUpDetails(e){
     e.preventDefault();
     if(e.target.classList.contains('kit-btn')){
         let makeItem = e.target.parentElement.parentElement;
-        fetch(`http://makeup-api.herokuapp.com/api/v1/products.json?id=${makeItem.id}`)
+       
+        fetch(`http://makeup-api.herokuapp.com/api/v1/products.json?${makeItem.id}`)
         .then(response => response.json())
-        .then(data => makeUpModal(data));
+        .then(makeUp => makeUpModal(makeUp));
     }
 }
 
+
 // create a modal
-function makeUpModal(post){
-    console.log(post)
-    post=post[0];
+function makeUpModal(makeUp){
+    console.log(makeUp)
+    makeUp=makeUp[0];
     
    
     let html = `
-        <h2 class = "makeup-title">${post.name}</h2>
-        <p class = "makeup-category">${post.brand}</p>
+        <h2 class = "makeup-title">${makeUp.name}</h2>
+        <p class = "makeup-category">${makeUp.brand}</p>
         <div class = "makeup-instruct">
             <h3>Instructions:</h3>
-            <p>${post.description}</p>
+            <p>${makeUp.description}</p>
         </div>
         <div class = "makeup-kit-img">
-            <img src = "${post.image_link}" alt = "">
+            <img src = "${makeUp.image_link}" alt = "">
         </div>
         <div class = "makeup-link">
-            <a href = "${post.product_link}" target = "_blank">product_link</a>
+            <a href = "${makeUp.product_link}" target = "_blank">product_link</a>
         </div>
     `;
     makeUpDetailsContent.innerHTML = html;
     makeUpDetailsContent.parentElement.classList.add('showMakeUp');
 }
+
